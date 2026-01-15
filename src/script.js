b2615e5bef7d95cabfed51c6f934f544d706052c
interluebke <111441242+interluebke@users.noreply.github.com>
2026-01-15 13:22:20 +0100

    Made box size in grid responsive

--- src/script.js
+++ src/script.js
@@ -7,11 +7,12 @@ function grid(size) {
         row.style.display = "flex";
         for (let j = 0; j < size; j++) {
             const newDiv = document.createElement("div");
-            newDiv.setAttribute("class", "box");
+            newDiv.setAttribute("class", "box");        
             newDiv.style.border = "1px solid black";
-            newDiv.style.height = "20px";
-            newDiv.style.width = "20px";
+            newDiv.style.width = `${960 / size}px`;
+            newDiv.style.aspectRatio = "1 / 1";
             newDiv.style.background = "white";
+            newDiv.style.boxSizing = "border-box";
             newDiv.addEventListener("mouseover", () => newDiv.style.background = "black");
             row.appendChild(newDiv);
         }
@@ -19,4 +20,4 @@ function grid(size) {
     }
 }
 
-grid(16);
+grid(100);
