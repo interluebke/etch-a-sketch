1de9b45643cf6f1eb603819525aab3f2ffec3821
interluebke <111441242+interluebke@users.noreply.github.com>
2026-01-15 11:57:20 +0100

    Expand grid() function
    Added an Event Listener to newly created boxes that turns them black
    when hovering over them

--- src/script.js
+++ src/script.js
@@ -7,9 +7,12 @@ function grid(size) {
         row.style.display = "flex";
         for (let j = 0; j < size; j++) {
             const newDiv = document.createElement("div");
+            newDiv.setAttribute("class", "box");
             newDiv.style.border = "1px solid black";
-            newDiv.style.height = "5px";
-            newDiv.style.width = "5px";
+            newDiv.style.height = "10px";
+            newDiv.style.width = "10px";
+            newDiv.style.background = "white";
+            newDiv.addEventListener("mouseover", () => newDiv.style.background = "black");
             row.appendChild(newDiv);
         }
         container.appendChild(row);
