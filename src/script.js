3fec4d478155853b7a40bb3f9af3ff5e8cf04bd8
interluebke <111441242+interluebke@users.noreply.github.com>
2026-01-15 11:28:57 +0100

    Complete grid function
    Extended the grid function to form the created divs into an actual grid

--- src/script.js
+++ src/script.js
@@ -2,13 +2,17 @@ const container = document.querySelector("#grid")
 
 function grid(size) {
     for(let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+        row.style.display = "flex";
         for (let j = 0; j < size; j++) {
             const newDiv = document.createElement("div");
             newDiv.style.border = "1px solid black";
             newDiv.style.height = "5px";
             newDiv.style.width = "5px";
-            container.appendChild(newDiv);
+            row.appendChild(newDiv);
         }
+        container.appendChild(row);
     }
 }
 
